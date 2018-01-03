86b086e1812161d78c1f8b7a675d7e72c6ca9074
muyuan.zhai <muyuan.zhai@datayes.com>
2018-01-03 20:20:18 +0800

    Myron: add take care of exception.

--- utils/error_utils.py
+++ utils/error_utils.py
@@ -2,9 +2,35 @@
 # **********************************************************************************#
 #     File: error utils
 # **********************************************************************************#
+import traceback
+from configs import logger
 
 
 error = (lambda code, message: {'code': code, 'data': message})
+
+
+def _logging_exception():
+    """
+    Logging exception
+    """
+    logger.error(traceback.format_exc())
+
+
+def take_care_of_exception(func):
+    """
+    Decorator: Deal with exception
+    """
+    def _decorator(obj, *args, **kwargs):
+        try:
+            response = func(obj, *args, **kwargs)
+        except tuple(Errors.error_types()), error_code:
+            _logging_exception()
+            response = error_code.args[0]
+        except:
+            _logging_exception()
+            response = error(500, '[{}] Exceptions.'.format(func.func_name))
+        return response
+    return _decorator
 
 
 class SchemaException(Exception):
