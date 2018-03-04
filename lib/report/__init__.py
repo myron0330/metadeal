6c92ceacb5d4e77f3ce0e1f8e11e2d6682d5542a
Myron <zhainandb@sina.com>
2018-03-04 10:23:39 +0800

    Update __init__.py

--- lib/report/__init__.py
+++ lib/report/__init__.py
@@ -1,18 +1,1 @@
-# -*- coding: utf-8 -*-
 
-"""
-__init__.py
-
-initialize report module
-
-@author: yudi.wu
-"""
-
-from . report import SecurityReport
-from . perf_parse import perf_parse
-
-
-__all__ = [
-    'SecurityReport',
-    'perf_parse'
-]
