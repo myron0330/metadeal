793889fec6a7b7d4c6344dcd0c4b26bd30e0f2e0
Myron <zhainandb@sina.com>
2018-03-04 10:11:55 +0800

    Update __init__.py

--- lib/account/__init__.py
+++ lib/account/__init__.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
-import fake_account
 from . base_account import BaseAccount
-from . stock_account import StockAccount
 from . futures_account import FuturesAccount
-from . otc_fund_account import OTCFundAccount
-from . index_account import IndexAccount
+
 
 __all__ = [
     'BaseAccount',
-    'StockAccount',
-    'FuturesAccount',
-    'OTCFundAccount',
-    'IndexAccount'
+    'FuturesAccount'
 ]
