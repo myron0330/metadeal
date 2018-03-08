4e1eee3c8f0aee856fe6632a7f4fe8127c392802
Myron <zhainandb@sina.com>
2018-03-08 17:20:14 +0800

    Update trading.py

--- lib/trading.py
+++ lib/trading.py
@@ -13,7 +13,7 @@ from . gateway import (
     PMSGateway,
     StrategyGateway
 )
-from . market import SubscriptionEngine
+from . subscriber import SubscriptionEngine
 from . trading_engine import TradingEngine
 from . const import PRESET_KEYARGS
 
@@ -112,9 +112,13 @@ def trading(strategy_code, config=None, **kwargs):
     data_portal = DataPortal()
     data_portal.batch_load_data(sim_params, disable_service=['market_service'])
     event_engine = EventEngine()
+    subscriber_engine = SubscriptionEngine.from_config(sim_params=sim_params)
     strategy_gateway = StrategyGateway()
-    pms_gateway = PMSGateway.from_config(clock, sim_params, data_portal, sim_params.accounts)
-    account_manager = AccountManager.from_config(clock, sim_params, data_portal, event_engine=event_engine)
+    pms_gateway = PMSGateway.from_config(clock, sim_params, data_portal,
+                                         subscriber_engine=subscriber_engine)
+    account_manager = AccountManager.from_config(clock, sim_params, data_portal,
+                                                 event_engine=event_engine,
+                                                 pms_gateway=pms_gateway)
     context = Context(clock, sim_params, strategy,
                       market_service=data_portal.market_service,
                       universe_service=data_portal.universe_service,
@@ -123,7 +127,7 @@ def trading(strategy_code, config=None, **kwargs):
                       account_manager=account_manager)
     trading_agent = TradingEngine(clock, sim_params, strategy,
                                   data_portal, context, account_manager,
-                                  subscription_engine=SubscriptionEngine,
+                                  subscription_engine=subscriber_engine,
                                   strategy_gateway=strategy_gateway,
                                   event_engine=event_engine,
                                   pms_gateway=pms_gateway)
