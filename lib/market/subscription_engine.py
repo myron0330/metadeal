4104748d712eea8daa920b11963216efd51b38b4
Myron <zhainandb@sina.com>
2018-03-08 17:16:00 +0800

    Update subscription_engine.py

--- lib/market/subscription_engine.py
+++ lib/market/subscription_engine.py
@@ -3,6 +3,8 @@
 #     File:
 # **********************************************************************************#
 from web.market.market import *
+from . position_subscriber import PositionSubscriber
+from . trade_subscriber import TradesSubscriber
 
 
 class MarketType(object):
@@ -19,10 +21,41 @@ market_type_map = {
 
 class SubscriptionEngine(object):
 
+    def __init__(self, account_ids=None,
+                 position_subscriber_pool=None,
+                 trade_subscriber_pool=None):
+        """
+        Args:
+            account_ids(list): account ids
+            position_subscriber_pool(dict): account_id --> position_subscriber
+            trade_subscriber_pool(dict): account_id --> trade_subscriber
+        """
+        self.account_ids = account_ids
+        self.position_subscriber_pool = position_subscriber_pool
+        self.trade_subscriber_pool = trade_subscriber_pool
+
+    @classmethod
+    def from_config(cls, sim_params=None, **kwargs):
+        """
+        Generate from parameters and web cache.
+
+        Args:
+            sim_params(obj): sim parameters
+        """
+        account_ids = {config.account_id for _, config in sim_params.accounts.iteritems()}
+        position_subscriber_pool = dict()
+        trade_subscriber_pool = dict()
+        for account_id in account_ids:
+            position_subscriber_pool[account_id] = PositionSubscriber(account_id)
+            trade_subscriber_pool[account_id] = TradesSubscriber(account_id)
+        return cls(account_ids=sim_params,
+                   position_subscriber_pool=position_subscriber_pool,
+                   trade_subscriber_pool=trade_subscriber_pool)
+
     @classmethod
     def fetch_market_quote(cls, market_type):
         """
-        Fetch market data base on a specific market type
+        Fetch market data base on a specific market type.
 
         Args:
             market_type(string): market type string
@@ -31,9 +64,17 @@ class SubscriptionEngine(object):
         for data in market_quote.fetch_data():
             yield data
 
-    @classmethod
-    def fetch_trade_response(cls):
+    def query_position_detail(self, account_id):
         """
-        Fetch trade response
+        Query position detail of account id.
+
+        Args:
+            account_id(string): account id
+        """
+        return self.position_subscriber_pool[account_id].fetch_data()
+
+    def fetch_trade_response(self):
+        """
+        Fetch trade response.
         """
         raise NotImplementedError
